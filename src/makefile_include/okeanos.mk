342a004a78eea5837136e2cb02684ed29b03b821
Grzegorz Bokota <bokota@gmail.com>
2016-06-22 15:16:31 +0200

    add compile option for okeanos

---
+++ src/makefile_include/okeanos.mk
@@ -0,0 +1,13 @@
+# okeanos supercomputer
+SYSTYPE = okeanos
+
+ZOLTANLIB=-L/apps/zoltan/3.83/INTEL/16.0/lib -lzoltan
+ZOLTANINC=-I/apps/zoltan/3.83/INTEL/16.0/include
+
+SPRNGLIB=-L/apps/sprng/2.0/INTEL/16.0/lib -lsprng
+SPRNGINC=-I/apps/sprng/2.0/INTEL/16.0/include
+
+HYPRELIB=-L/apps/hypre/2.11.0/INTEL/16.0/lib -lHYPRE
+HYPREINC=-I/apps/hypre/2.11.0/INTEL/16.0/include
+
+GMPLIB=-L/apps/gmp/6.1.0/INTEL/16.0/lib -lgmp
